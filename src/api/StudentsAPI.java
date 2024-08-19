ea0baab40679638ac3e65cdfd02180f959f99b8c
Murilo Unten <murilo.unten@proton.me>
2024-08-19 18:34:16 -0300

    barebones api layer

--- src/api/StudentsAPI.java
+++ src/api/StudentsAPI.java
@@ -1,8 +1,48 @@
 package api;
 
-import src.storage.*;
+import storage.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.security.cert.TrustAnchor;
+import java.sql.SQLException;
 
 public class StudentsAPI {
     private Storage db;
 
+    public StudentsAPI(String user, String password) throws SQLException {
+        db = new Storage(user, password);
+    }
+
+    public Optional<ArrayList<Student>> listStudents() {
+        Optional<ArrayList<Student>> output;
+        try {
+            ArrayList<Student> students = db.getStudents();
+            output = Optional.ofNullable(students);
+        }
+        catch (Exception e) {
+            output = Optional.ofNullable(null);
+        }
+
+        return output;
+    }
+
+    public void createStudent(String name) {
+        try {
+            db.createStudent(name);
+            System.out.println("Aluno criado com sucesso.");
+        }
+        catch (Exception e) {
+            System.out.println("Não foi possível criar o aluno. Por favor tente novamente.");
+        }
+    }
+
+    public void deleteStudent(int id) {
+        try {
+            db.deleteStudent(id);
+        }
+        catch (Exception e) {
+            System.out.println("Não foi possível deletar o aluno. Por favor tente novamente.");
+        }
+    }
 }
